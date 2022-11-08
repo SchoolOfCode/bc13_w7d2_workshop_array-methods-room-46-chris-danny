4857d740a184a8cd58886e11bba621514cc777f0
C0dingChris <Chris.Sanger@outlook.com>
2022-11-08 12:27:23 +0000

    Task 1 Complete!

--- task1.js
+++ task1.js
@@ -1,3 +1,14 @@
+// 1a. Write a function that takes in a string and turns it to upper case.
+
+// 👉 1b. Use the `.map()` method to generate a new array by calling your function on each item in `cats`. Store the returned value from map in the variable `capitalisedCats`,
+
+
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map(function(name){
+    
+    return name.toUpperCase()
+    
+    
+})
+
